f9425fe7ba97f20a02a2b68bedfc8c9649656dc0
mehreennajm <mehreen.najm@gmail.com>
2022-10-14 12:48:16 +0200

    updated

--- src/app/components/_services/auth.service.ts
+++ src/app/components/_services/auth.service.ts
@@ -22,28 +22,6 @@ export class AuthService {
     return this.httpClient.post(this.PATH_OF_API+ '/authenticate',loginData,{headers:this.requestHeader,});
   }
 
-  // public roleMatch(allowedRole:string[]): boolean | undefined {
-  //   let isMatch = false;
-  //   const userRole: string[] = this.userAuthService.getRoles();
-  //   try {
-  //       allowedRole.forEach((element)=>{
-  //         console.log(element)
-  //       })
-  //       if (userRole != null && userRole==allowedRole)
-  //       {
-  //           isMatch = true;
-  //           return isMatch
-          
-  //       }
-  //       else{
-  //         return isMatch;
-  //       }
-  //  } catch (err) {
-  //    console.log(err);
-  //  }
-    
-  // }
-
   public roleMatch(allowedRoles:any) :boolean{
     let isMatch = false;
     const userRoles: any = this.userAuthService.getRoles();
